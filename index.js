48087dbf9bec5678b136dbb8f062a0efda6093ce
PetePhx <pete0phx@yandex.com>
2018-10-27 10:43:17 -0700

    minor refactor

--- index.js
+++ index.js
@@ -20,18 +20,15 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) res.status(404)
-									.send('The course with the given id was not found.');
+	if (!course) return res.status(404)
+												 .send('The course with the given id was not found.');
 	res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
 	const { error } = validateCourse(req.body);
-	if (error) {
-		res.status(400)
-			 .send(error.details[0].message);
-		return;
-	}
+	if (error)	return res.status(400)
+			 					 				.send(error.details[0].message);
 	
 	let course = {
 		id: courses.length + 1,
@@ -45,19 +42,13 @@ app.post('/api/courses', (req, res) => {
 app.put('/api/courses/:id', (req, res) => {
 	// Look up the course, if non existant, return 404 not found
 	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) {
-		res.status(404)
-			 .send('The course with the given id was not found.');
-		return;
-	}
+	if (!course) return res.status(404)
+			 									 .send('The course with the given id was not found.');
 									
 	// Validate the updates, if invalid, return 400 bad request
 	const { error } = validateCourse(req.body);
-	if (error) {
-		res.status(400)
-			 .send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400)
+											 .send(error.details[0].message);
 
 	// Update and return the updated course
 	course.name = req.body.name;
@@ -66,11 +57,8 @@ app.put('/api/courses/:id', (req, res) => {
 
 app.delete('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) {
-		res.status(404)
-			 .send('The course with the given id was not found.');
-		return;
-	}
+	if (!course) return res.status(404)
+			 									 .send('The course with the given id was not found.');
 
 	const index = courses.indexOf(course);
 	courses.splice(index, 1);
