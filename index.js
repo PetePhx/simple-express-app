66aa5220b8b98fec36fec41a5dbc2534f493d7f1
PetePhx <pete0phx@yandex.com>
2018-10-26 20:00:47 -0700

    basic input validation

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+	if (!req.body.name || req.body.name.length < 3) {
+		// 400 bad request
+		res.status(400)
+			 .send('Name is required -- minimum 3 characters.');
+		return;
+	}
+	
 	let course = {
 		id: courses.length + 1,
 		name: req.body.name,
