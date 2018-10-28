3199ac78067005adb9b0e25c480cefefc9358949
PetePhx <pete0phx@yandex.com>
2018-10-28 14:25:18 -0700

    add middlewares: helmet, morgan

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
 const logger = require('./logger');
 const authenticator = require('./authenticator');
@@ -7,9 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.use(logger);
-
 app.use(authenticator);
 
 const courses = [
