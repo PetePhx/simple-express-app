cb08f55f09163d62fa77ffc41b5169ca61bc0892
PetePhx <pete0phx@yandex.com>
2018-10-28 14:03:16 -0700

    basic middleware

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const Joi = require('joi');
+const logger = require('./logger');
+const authenticator = require('./authenticator');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.use(logger);
+
+app.use(authenticator);
 
 const courses = [
 	{id: 1, name: 'course 1'},
